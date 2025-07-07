1d09aa2e92014a0d46541ad4da2820d0b697ada0
B1gN0Se <86577632+B1gN0Se@users.noreply.github.com>
2025-07-07 15:00:54 -0300

    Create download_repo.py

---
+++ download_repo.py
@@ -0,0 +1,9 @@
+import requests
+import os
+
+user = "nome-do-usuario"
+url = f"https://api.github.com/users/b1gn0se/repos?per_page=100&type=all"
+repos = requests.get(url).json()
+
+for repo in repos:
+    os.system(f"git clone {repo['clone_url']}")
